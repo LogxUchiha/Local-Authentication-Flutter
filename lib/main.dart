3202c66b2220e3b6001adc90316c3f4ff9375b99
LogxUchiha <logx03@yahoo.com>
2023-09-02 04:41:58 +0800

    UPDATED MAIN

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 
 import 'page/fingerprint_page.dart';
 
