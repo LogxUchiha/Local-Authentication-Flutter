1afca3018602675ad5ec022d77a915da3c3a596a
LogxUchiha <logx03@yahoo.com>
2023-09-02 04:29:39 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
+
+import 'page/fingerprint_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+
+  //code for no screenshot
+  WidgetsBinding.instance.addPostFrameCallback((timeStamp) async{
+    if (Platform.isAndroid){
+      await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+    }
+   });
+  //code for no screenshot 
+
+
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Local Authentication ';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: FingerprintPage(),
+      );
+}
