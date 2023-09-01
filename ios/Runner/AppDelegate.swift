6aabd63ad3119b8ac26d625c3a3c134ca188d0e0
The logxVance <73808428+LogxUchiha@users.noreply.github.com>
2023-09-02 04:41:30 +0800

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,15 +7,11 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-<<<<<<< HEAD
     self.window.secureApp()
-=======
->>>>>>> eb8b173cf7bcd83ed6938a548b08f8abe1bc41e3
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-<<<<<<< HEAD
 
 extension UIWindow {
   func secureApp(){
@@ -29,5 +25,4 @@ extension UIWindow {
       
 }
 }
-=======
->>>>>>> eb8b173cf7bcd83ed6938a548b08f8abe1bc41e3
+
